d223bc2c23235b933064b6759f2758fee7825c85
Raiffe <1993.raiffe@gmail.com>
2025-03-03 16:03:46 -0300

    fixing bugs

--- src/components/AppsScreen.jsx
+++ src/components/AppsScreen.jsx
@@ -1,13 +1,12 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { PageContext } from '../context/PageContext';
-import { Link } from 'react-router-dom';
-import CalendarApp from './CalendarApp';
-import iconConfig from './iconConfig';
-import { useTranslation } from 'react-i18next';
-import WidgetBinance from './WidgetBinance.jsx';
-import WidgetStocks from './WidgetStocks.jsx';
-import '../styles/widget.css';
-import axios from 'axios';
+import React, { useState, useContext, useEffect } from "react";
+import { PageContext } from "../context/PageContext";
+import { Link } from "react-router-dom";
+import CalendarApp from "./CalendarApp";
+import iconConfig from "./iconConfig";
+import { useTranslation } from "react-i18next";
+import WidgetBinance from "./WidgetBinance.jsx";
+import WidgetStocks from "./WidgetStocks.jsx";
+import "../styles/widget.css";
 
 const AppsScreen = () => {
   const [isFirstPage, setIsFirstPage] = useState(true);
@@ -18,7 +17,7 @@ const AppsScreen = () => {
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       console.log(
-        'Ei, estou contando com seu comentário no Feedback App! Thanks!',
+        "Ei, estou contando com seu comentário no Feedback App! Thanks!"
       );
     }, 2000);
 
@@ -30,10 +29,10 @@ const AppsScreen = () => {
         <div className="img-hover">
           <img src={app} alt={app} />
         </div>
-        {appName === 'memory game' &&
-        localStorage.getItem('language') === 'ptBR' ? (
+        {appName === "memory game" &&
+        localStorage.getItem("language") === "ptBR" ? (
           <p
-            style={{ fontSize: '10px', marginTop: '2px' }}
+            style={{ fontSize: "10px", marginTop: "2px" }}
             className="apps-text"
           >
             {t(appName)}
@@ -45,24 +44,27 @@ const AppsScreen = () => {
     );
   }
 
-  const handleClickPages = () => {
+  /*const handleClickPages = () => {
     setIsFirstPage(!isFirstPage);
   };
 
-  /*const handleGoogleMapsClick = () => {
-    clickCounter('googleMaps');
+  const handleGoogleMapsClick = () => {
+   
     window.location.reload();
     window.location.href = '/Google-Maps';
-  };*/
+  };
 
   const clickCounter = (app) => {
     axios
+      .post('http://localhost:5000/clicks', {
       .post('http://localhost:5000/clicks', {
         appName: app,
       })
       .then((res) => {
         if (app === 'instagram' || app === 'github' || app === 'linkedin') {
+        if (app === 'instagram' || app === 'github' || app === 'linkedin') {
           return;
+        } else if (app === 'spotify' || app === 'Memory Game') {
         } else if (app === 'spotify' || app === 'Memory Game') {
           return;
         } else {
@@ -72,186 +74,85 @@ const AppsScreen = () => {
       .catch((err) => {
         console.log(err);
       });
-  };
+  };*/
 
   return (
     <div className="apps-screen">
-      <div
-        onClick={handleClickPages}
-        className={`apps ${isSlided ? 'slide3' : 'slide'}`}
-        id="APPS"
-      >
+      <div className={`apps ${isSlided ? "slide3" : "slide"}`} id="APPS">
         <div className="apps-line">
-          <Link
-            onClick={(event) => {
-              clickCounter('mail');
-            }}
-            to={'/Mail'}
-          >
-            {apps(iconConfig.mailApp, 'mail')}
-          </Link>
-          <Link
-            onClick={(event) => {
-              clickCounter('calendar');
-            }}
-            to={'/Calendar'}
-          >
+          <Link to={"/Mail"}>{apps(iconConfig.mailApp, "mail")}</Link>
+          <Link to={"/Calendar"}>
             <CalendarApp />
           </Link>
-          <Link
-            onClick={(event) => {
-              clickCounter('photos');
-            }}
-            to={'/Photos'}
-          >
-            {apps(iconConfig.photosApp, 'photos')}
+          <Link to={"/Photos"}>{apps(iconConfig.photosApp, "photos")}</Link>
+          <Link to={"/Camera"}>{apps(iconConfig.cameraApp, "camera")}</Link>
+        </div>
+        <div className="apps-line">
+          <Link to={"/FaceTime"}>
+            {apps(iconConfig.facetimeApp, "FaceTime")}
           </Link>
-          <Link
-            onClick={(event) => {
-              clickCounter('camera');
-            }}
-            to={'/Camera'}
-          >
-            {apps(iconConfig.cameraApp, 'camera')}
+          <Link to={"/Clock"}>{apps(iconConfig.clockApp, "clock")}</Link>
+          <Link to={"/Weather"}>{apps(iconConfig.weatherApp, "weather")}</Link>
+          <Link to={"/Reminders"}>
+            {apps(iconConfig.remindersApp, "reminders")}
           </Link>
         </div>
         <div className="apps-line">
-          <Link
-            onClick={(event) => {
-              clickCounter('facetime');
-            }}
-            to={'/FaceTime'}
-          >
-            {apps(iconConfig.facetimeApp, 'FaceTime')}
+          <Link to={"/Notes"}>{apps(iconConfig.notesApp, "notes")}</Link>
+          <Link to={"/Stocks"}>{apps(iconConfig.stocksApp, "stocks")}</Link>
+          <Link to={"/AppStore"}>
+            {apps(iconConfig.appStoreApp, "App Store")}
           </Link>
-          <Link
-            onClick={(event) => {
-              clickCounter('clock');
-            }}
-            to={'/Clock'}
-          >
-            {apps(iconConfig.clockApp, 'clock')}
-          </Link>
-          <Link
-            onClick={(event) => {
-              clickCounter('weather');
-            }}
-            to={'/Weather'}
-          >
-            {apps(iconConfig.weatherApp, 'weather')}
-          </Link>
-          <Link
-            onClick={(event) => {
-              clickCounter('reminders');
-            }}
-            to={'/Reminders'}
-          >
-            {apps(iconConfig.remindersApp, 'reminders')}
-          </Link>
-        </div>
-        <div className="apps-line">
-          <Link
-            to={'/Notes'}
-            onClick={(event) => {
-              clickCounter('notes');
-            }}
-          >
-            {apps(iconConfig.notesApp, 'notes')}
-          </Link>
-          <Link
-            onClick={(event) => {
-              clickCounter('stocks');
-            }}
-            to={'/Stocks'}
-          >
-            {apps(iconConfig.stocksApp, 'stocks')}
-          </Link>
-          <Link
-            onClick={(event) => {
-              clickCounter('appStore');
-            }}
-            to={'/AppStore'}
-          >
-            {apps(iconConfig.appStoreApp, 'App Store')}
-          </Link>
-          <Link
-            onClick={(event) => {
-              clickCounter('calculator');
-            }}
-            to={'/Calculator'}
-          >
-            {apps(iconConfig.calculatorApp, 'calculator')}
+          <Link to={"/Calculator"}>
+            {apps(iconConfig.calculatorApp, "calculator")}
           </Link>
 
           {/*<Link
-            onClick={() => {
-              clickCounter("feedback");
-            }}
+            
             to={"/Feedback"}
           >
             {apps(iconConfig.feedbackApp, "Feedback")}
+          </Link>
           </Link>*/}
         </div>
+
         <div className="apps-line">
-          <Link
-            onClick={(event) => clickCounter('memoryGame')}
-            to={'/memoryGame'}
-          >
-            {apps(iconConfig.memoryGameApp, 'memory game')}
+          <Link to={"/memoryGame"}>
+            {apps(iconConfig.memoryGameApp, "memory game")}
           </Link>
 
-          <Link
-            onClick={(event) => clickCounter('ticTacToe')}
-            to={'/ticTacToe'}
-          >
-            {apps(iconConfig.ticTacToeApp, 'ticTacToe')}
+          <Link to={"/ticTacToe"}>
+            {apps(iconConfig.ticTacToeApp, "ticTacToe")}
           </Link>
 
           {/*<div onClick={handleGoogleMapsClick}>
             {apps(iconConfig.googleMapsApp, 'Google Maps')}
           </div>*/}
-          <Link onClick={(event) => clickCounter('contacts')} to={'/Contacts'}>
-            {apps(iconConfig.contactsApp, 'contacts')}
+          <Link to={"/Contacts"}>
+            {apps(iconConfig.contactsApp, "contacts")}
           </Link>
-          <Link onClick={(event) => clickCounter('settings')} to={'/Settings'}>
-            {apps(iconConfig.settingsApp, 'settings')}
+          <Link to={"/Settings"}>
+            {apps(iconConfig.settingsApp, "settings")}
           </Link>
         </div>
+
         <div className="apps-line">
-          <Link
-            onClick={(event) => clickCounter('spotify')}
-            to={'/Spotify/Peregrino'}
-          >
-            {apps(iconConfig.spotifyApp, 'Spotify')}
+          {/*<Link to={"/Spotify/Peregrino"}>
+            {apps(iconConfig.spotifyApp, "Spotify")}
+          </Link> */}
+          <Link target="_blank" to={"https://www.linkedin.com/in/raiffemoura/"}>
+            {apps(iconConfig.linkedinApp, "LinkedIn")}
           </Link>
-          <Link
-            onClick={(event) => clickCounter('linkedin')}
-            target="_blank"
-            to={'https://www.linkedin.com/in/raiffemoura/'}
-          >
-            {apps(iconConfig.linkedinApp, 'LinkedIn')}
+          <Link target="_blank" to={"http://instagram.com/raiffemoura"}>
+            {apps(iconConfig.instagramApp, "Instagram")}
           </Link>
-          <Link
-            onClick={(event) => clickCounter('instagram')}
-            target="_blank"
-            to={'http://instagram.com/raiffemoura'}
-          >
-            {apps(iconConfig.instagramApp, 'Instagram')}
+          <Link target="_blank" to={"https://github.com/raiffemoura"}>
+            {apps(iconConfig.githubApp, "Github")}
           </Link>
-          <Link
-            onClick={(event) => clickCounter('github')}
-            target="_blank"
-            to={'https://github.com/raiffemoura'}
-          >
-            {apps(iconConfig.githubApp, 'Github')}
-          </Link>
+          <div className="app-space"></div>
         </div>
       </div>
-      <div
-        onClick={handleClickPages}
-        className={`apps ${isSlided ? 'slide4' : 'slide2'}`}
-        id="APPS2"
-      >
+      <div className={`apps ${isSlided ? "slide4" : "slide2"}`} id="APPS2">
         <div className="widget">
           <WidgetBinance />
         </div>
@@ -263,19 +164,21 @@ const AppsScreen = () => {
 
         <div className="apps-line">
           {/*<Link
-            onClick={(event) => clickCounter('ticTacToe')}
+           
             to={'/ticTacToe'}
           >
             {apps(iconConfig.ticTacToeApp, 'ticTacToe')}
+            {apps(iconConfig.ticTacToeApp, 'ticTacToe')}
           </Link>
           <Link
-            onClick={(event) => clickCounter('memoryGame')}
+           }
             to={'/memoryGame'}
           >
             {apps(iconConfig.memoryGameApp, 'memory game')}
+            {apps(iconConfig.memoryGameApp, 'memory game')}
           </Link>
 
-          <Link onClick={(event) => clickCounter('resizer')} to={'/resizer'}>
+          <Link  to={'/resizer'}>
             {apps(iconConfig.resizerApp, 'resizer')}
           </Link>*/}
         </div>
